b2b188ad1b59ddd37eee78219c44de6e5421994b
mokriiiii <forproga@yandex.ru>
2023-11-28 08:10:55 +0300

    Fix pep8 4.0

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -7,8 +7,8 @@ class AdminOrAuthorPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (
-            request.method in permissions.SAFE_METHODS or
-            request.user.is_authenticated
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
